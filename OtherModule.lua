96ddbcfecf3f80180ebbfec696d794947ea2cad6
Gizzmo <justgiz@gmail.com>
2018-02-25 23:09:24 -0800

    Trim up OtherModule
    Stript it down to the barebones

--- OtherModule.lua
+++ OtherModule.lua
@@ -9,19 +9,12 @@ local L = Addon.L
 local defaultDB = {
     profile = {
 
-    },
-    global = {
-
     }
 }
 
 ---------------------------------------------------------------- Core Methods --
 function Module:OnInitialize()
     self.db = Addon.db:RegisterNamespace(MODULE_NAME, defaultDB)
-
-    self.db.RegisterCallback(self, "OnProfileChanged", "OnProfileRefresh")
-    self.db.RegisterCallback(self, "OnProfileCopied", "OnProfileRefresh")
-    self.db.RegisterCallback(self, "OnProfileReset", "OnProfileRefresh")
 
     self:Print('OnInitialize Trigered')
 end
@@ -32,10 +25,6 @@ end
 
 function Module:OnDisable()
     self:Print('OnDisable Trigered')
-end
-
-function Module:OnProfileRefresh()
-    self:Print('OnProfileRefresh Trigered')
 end
 
 ------------------------------------------------------------------------- Fin --
